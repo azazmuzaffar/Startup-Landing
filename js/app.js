b597b8b953310727e28f7989b70cfc398461d3b3
Azaz Moon <Sp18-bcs-cuiwah.edu.pk>
2021-10-02 00:16:07 +0500

    First 2 Sections are done (Desktop Version)

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
-$(document).ready(function () {
-  $(".--video--button--").click(function () {
-    $(".--video--").css("display", "block");
-    $(".--frame--").animate({ opacity: "1", "margin-top": "0px" }, 200);
-  });
-});
-$(document).ready(function () {
-  $(".--close--").click(function () {
-    $(".--video--").css("display", "none");
-    $(".--frame--").animate({ opacity: "1", "margin-top": "70px" }, 200);
-  });
-});
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>>>>>>>>>> Navbar <<<<<<<<<<<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
 
+/* Make the navbar fixed on scroll > 200 */
 $(window).scroll(function () {
   var nav = $(".--navigation--");
   var top = 200;
@@ -19,4 +11,23 @@ $(window).scroll(function () {
   } else {
     nav.removeClass("fixed");
   }
+});
+
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+/* >>>>>>>>>>>>>>> Video <<<<<<<<<<<<<< */
+/* >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<  */
+
+/* It will play the Video */
+$(document).ready(function () {
+  $(".--video--button--").click(function () {
+    $(".--video--").css("display", "block");
+    $(".--frame--").animate({ opacity: "1", "margin-top": "0px" }, 200);
+  });
+});
+/* It will hide the Video */
+$(document).ready(function () {
+  $(".--close--").click(function () {
+    $(".--video--").css("display", "none");
+    $(".--frame--").animate({ opacity: "1", "margin-top": "70px" }, 200);
+  });
 });
